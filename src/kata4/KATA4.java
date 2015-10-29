c303ff36b1a78953f0e351b9ee2a37a1adb18fb5
joaquin <joaquin@joaquin-Notebook>
2015-10-29 09:22:15 +0000

    Version 2 KATA 4, copia ficheros con buffer, muchos mas rapido

--- src/kata4/KATA4.java
+++ src/kata4/KATA4.java
@@ -25,11 +25,13 @@ public class KATA4 {
         BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
         BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
         
+        byte [] buffer= new byte[1024];
+        
         Date date=new Date();
         while(true){
-            int read= input.read();
+            int read= input.read(buffer);
             if(read<0) break;
-            output.write(read);
+            output.write(buffer);
         }
         System.out.println(new Date().getTime()-date.getTime()+ "msg");
         input.close();
